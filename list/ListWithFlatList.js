40d3337a8da5bfad897a6a5e38e7233a322ead9b
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-19 17:42:53 +0530

    feat : list with flat list

---
+++ list/ListWithFlatList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {FlatList, Text, View} from 'react-native';
+import style from '../style';
+
+const ListData = () => {
+  const user = [
+    {
+      id: 1,
+      name: 'Jhon',
+    },
+    {
+      id: 2,
+      name: 'Mike',
+    },
+    {
+      id: 3,
+      name: 'Bruce',
+    },
+    {
+      id: 4,
+      name: 'Phillip',
+    },
+    {
+      id: 5,
+      name: 'Glenn',
+    },
+  ];
+  return (
+    <View>
+      <Text style={style.text}>User List</Text>
+      <FlatList
+        data={user}
+        renderItem={({item}) => <Text style={style.list}>{item.name}</Text>}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
+};
+
+export default ListData;
