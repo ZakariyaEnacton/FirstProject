17eededdddd865cfe182aa0c1e59fba8ccdace55
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-22 11:25:07 +0530

    feat : user data external component

---
+++ components/UserData.js
@@ -0,0 +1,15 @@
+import {Text, View} from 'react-native';
+import style from '../style';
+
+const UserData = props => {
+  const getData = props.data;
+  return (
+    <View style={style.box}>
+      <Text style={style.item}>{getData.id}</Text>
+      <Text style={style.item}>{getData.name}</Text>
+      <Text style={style.item}>{getData.email}</Text>
+    </View>
+  );
+};
+
+export default UserData;
