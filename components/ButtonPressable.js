5f0aad6155da4d4a0b1f3e9c1c8a6a528a63a03d
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-25 17:50:13 +0530

    feat : pressable button

---
+++ components/ButtonPressable.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Pressable, View, Text} from 'react-native';
+import style from '../style';
+
+const PressBtn = () => {
+  return (
+    <View>
+      <Pressable
+        onPress={() => console.warn('normal press')}
+        onLongPress={() => console.warn('long press')}
+        onPressIn={() => console.warn('press in')}
+        onPressOut={() => console.warn('press out')}>
+        <Text style={style.pressBtn}>Pressable Button</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default PressBtn;
