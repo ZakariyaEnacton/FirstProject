7fef658690e2c588ce6e750bb89cdaef4f0331f9
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-25 13:43:34 +0530

    feat : button styling

---
+++ components/StyleWithButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {TouchableHighlight, View, Text} from 'react-native';
+import style from '../style';
+
+const StyleWithButton = () => {
+  return (
+    <View>
+      <TouchableHighlight>
+        <Text style={[style.button, style.primary]}>Primary</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[style.button, style.success]}>Success</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[style.button, style.warning]}>Warning</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[style.button, style.error]}>Error</Text>
+      </TouchableHighlight>
+    </View>
+  );
+};
+export default StyleWithButton;
