e56fbe2e5dbcdddad17fcf760fd761536bde8b68
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-25 13:43:55 +0530

    feat : layout responsive design

---
+++ Layoutdesign/Compo.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import style from '../style';
+
+const Compo = () => {
+  return (
+    <View style={{flex: 1}}>
+      <View style={style.box1}>
+        <View style={style.innerBox1}>
+          <View style={style.innerBox7}></View>
+          <View style={style.innerBox8}></View>
+          <View style={style.innerBox9}></View>
+        </View>
+        <View style={style.innerBox2}></View>
+        <View style={style.innerBox3}></View>
+      </View>
+      <View style={style.box2}>
+        <View style={style.innerBox4}></View>
+        <View style={style.innerBox5}></View>
+      </View>
+      <View style={style.box3}>
+        <View style={style.innerBox6}></View>
+      </View>
+    </View>
+  );
+};
+
+export default Compo;
