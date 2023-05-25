f6cb7e6bbd564b7ca6ac03e6fd3757a92a605a08
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-25 13:43:22 +0530

    feat : radio button in react native

---
+++ components/Radio.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+import style from '../style';
+
+const RadioButtonUI = () => {
+  const [selectRadio, setSelectRadio] = useState(1);
+
+  return (
+    <View style={style.radioMain}>
+      <TouchableOpacity onPress={() => setSelectRadio(1)}>
+        <View style={style.radioWrapper}>
+          <View style={style.radio}>
+            {selectRadio === 1 ? <View style={style.radioBg}></View> : null}
+          </View>
+          <Text style={style.radioText}>Radio Button 1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setSelectRadio(2)}>
+        <View style={style.radioWrapper}>
+          <View style={style.radio}>
+            {selectRadio === 2 ? <View style={style.radioBg}></View> : null}
+          </View>
+          <Text style={style.radioText}>Radio Button 1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setSelectRadio(3)}>
+        <View style={style.radioWrapper}>
+          <View style={style.radio}>
+            {selectRadio === 3 ? <View style={style.radioBg}></View> : null}
+          </View>
+          <Text style={style.radioText}>Radio Button 1</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default RadioButtonUI;
