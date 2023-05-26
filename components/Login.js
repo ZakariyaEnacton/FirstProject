9a3ed2a7429dca0910d48d8d37a3cdc8438c7bd2
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-26 12:42:56 +0530

    feat : navigator

---
+++ components/Login.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import {
+  Button,
+  Text,
+  TextInput,
+  Touchable,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+import InputVal from './InputTextValue';
+import style from '../style';
+import Home from './Home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Login = props => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  return (
+    <View>
+      <View style={style.loginFieldView}>
+        <Text style={style.loginText}>Name :</Text>
+        <TextInput
+          style={style.loginInputText}
+          placeholder="Enter Name"
+          onChangeText={myname => setName(myname)}
+          value={name}
+        />
+      </View>
+      <View style={style.loginFieldView}>
+        <Text style={style.loginText}>Email :</Text>
+        <TextInput
+          style={style.loginInputText}
+          placeholder="Enter email"
+          onChangeText={myemail => setEmail(myemail)}
+          value={email}
+        />
+      </View>
+      <View style={style.loginFieldView}>
+        <TouchableHighlight
+          underlayColor=""
+          onPress={() =>
+            props.navigation.navigate('Home', {
+              getName: name,
+              getEmail: email,
+            })
+          }>
+          <Text style={style.loginBtn}>Login</Text>
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
+};
+
+export default Login;
