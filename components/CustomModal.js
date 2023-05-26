90ba7b5e71e7eea1316cb565089d294dd5d5873f
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-26 09:42:02 +0530

    feat : design custom modal

---
+++ components/CustomModal.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import {Text, TouchableHighlight, View} from 'react-native';
+import style from '../style';
+
+const CustMod = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <View style={style.container}>
+      {show ? (
+        <View style={style.modal}>
+          <View style={style.modalBody}>
+            <Text style={style.modalT}>Hey! Custom Modal</Text>
+            <TouchableHighlight onPress={() => setShow(false)}>
+              <Text style={[style.openBtn, style.closeBtn]}>
+                Press to close
+              </Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      ) : null}
+      <TouchableHighlight onPress={() => setShow(true)}>
+        <Text style={style.openBtn}>Press to open</Text>
+      </TouchableHighlight>
+    </View>
+  );
+};
+
+export default CustMod;
