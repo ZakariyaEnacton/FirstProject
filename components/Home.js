dd0734fec14571085dbdd424bd9868ee2669abd4
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-26 12:42:42 +0530

    feat : login ui

---
+++ components/Home.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
+import style from '../style';
+
+const Home = ({route}) => {
+  return (
+    <View style={style.homeContainer}>
+      <View>
+        <Text style={style.homeHeading}>Welcome to Home Page </Text>
+        {/* {console.log('Props', props)} */}
+      </View>
+      <View style={style.homeInfo}>
+        <Text style={style.homeText}>Name : {route.params.getName}</Text>
+        <Text style={style.homeText}>Email : {route.params.getEmail}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
