ffaec88c6330c6eb2217c325d81b77c6e46a3909
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-29 13:34:52 +0530

    feat : update count

--- src/components/ButtonTouchableOpacity.js
+++ src/components/ButtonTouchableOpacity.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import touchableOpacityStyle from '../style/touchableOpacityStyle';
 
 const ButtonTouchableOpacity = () => {
+  const [count, setCount] = useState(0);
+
+  const countNumber = () => {
+    setCount(count + 1);
+  };
+
   return (
     <View>
-      <TouchableOpacity activeOpacity={0.5} style={tochableStyle.touchButton}>
+      <TouchableOpacity
+        onPress={countNumber}
+        activeOpacity={0.5}
+        style={tochableStyle.touchButton}>
         <Text style={touchableOpacityStyle.touchText}>Click Here</Text>
       </TouchableOpacity>
+
+      <Text style={{textAlign: 'center', fontSize: 24}}>{count}</Text>
     </View>
   );
 };
