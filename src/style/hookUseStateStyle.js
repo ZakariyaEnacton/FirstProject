4032e98a98b4f746b243a7259bd60dbb431da51c
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-29 16:21:04 +0530

    feat : style to useState Hook componeent

---
+++ src/style/hookUseStateStyle.js
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+export default stateStyle = StyleSheet.create({
+  button: {
+    backgroundColor: 'lightgreen',
+    alignItems: 'center',
+    width: 250,
+    padding: 10,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 500,
+    fontFamily: 'times new roman',
+  },
+  countText: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 20,
+    fontWeight: 500,
+    fontFamily: 'times new roman',
+  },
+});
