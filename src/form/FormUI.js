730bf7bf4d54569f355480cc91ee38d4975a3f0f
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-29 12:02:51 +0530

    feat : directory change

--- src/form/FormUI.js
+++ src/form/FormUI.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {Button, Text, TextInput, View} from 'react-native';
-import style from '../src/style/style';
-import {transformer} from '../metro.config';
+import style from '../style/style';
+import {transformer} from '../../metro.config';
 
 const UserForm = () => {
   const [name, setName] = useState('');
