4b76a0407495136fda12c617e19c3d11ed7c03a2
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-29 16:21:24 +0530

    feat : style to touchableWithoutFeedback Button

---
+++ src/style/touchableWithoutFeedback.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export default btnWithoutFeedback = StyleSheet.create({
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green',
+    padding: 10,
+    margin: 10,
+  },
+});
