6cae9072b33baa4ceb43b92c783719e982897c20
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-31 15:23:45 +0530

    feat : useMemo Hook styling

---
+++ src/style/MemoStyle.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export default memoStyle = StyleSheet.create({
+  button: {
+    backgroundColor: '#06712E',
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 14,
+    color: 'white',
+    textAlign: 'center',
+  },
+  text: {
+    margin: 10,
+    fontSize: 18,
+  },
+});
