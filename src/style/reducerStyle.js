d6a3fa669da708d7d14afd74243ba6f334b27378
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-31 17:05:23 +0530

    feat : reducer style

---
+++ src/style/reducerStyle.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export default reducerStyle = StyleSheet.create({
+  reducerBtnWrapper: {
+    flexDirection: 'row',
+  },
+  reducerBtnText: {
+    margin: 15,
+    fontSize: 50,
+  },
+});
