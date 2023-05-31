29324204f7dffcd9e8cd9da7e1814635438a995e
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-31 14:02:55 +0530

    feat : useLayoutEffect Hook style for component

---
+++ src/style/UseLayoutEffectStyle.js
@@ -0,0 +1,22 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+
+export default layoutEffect = StyleSheet.create({
+  effectTitle: {
+    fontSize: 25,
+    fontFamily: 'times new roman',
+    textAlign: 'center',
+  },
+  effectButton: {
+    backgroundColor: '#57846E',
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 10,
+  },
+  effectButtonText: {
+    fontSize: 18,
+    color: '#fff',
+    textAlign: 'center',
+    fontFamily: 'times new roman',
+  },
+});
