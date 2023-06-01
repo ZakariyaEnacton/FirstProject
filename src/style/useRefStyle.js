e1f8b08c4c83d6bd3c0bb4faf358409dfe1dc05b
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-01 11:04:25 +0530

    feat : useRef hook styling

---
+++ src/style/useRefStyle.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export default refStyle = StyleSheet.create({
+  refButtonText: {
+    fontSize: 18,
+    color: 'black',
+    fontWeight: 500,
+  },
+});
